9bba6de7f51882909e8299dc437233342a9589a9
vucv <vucv@scuti.asia>
2022-08-11 10:24:06 +0700

    unit test

--- truffle/contracts/TodoList.sol
+++ truffle/contracts/TodoList.sol
@@ -1,6 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.14;
 
 contract TodoList {
-  uint public taskCount = 0;
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(uint id, string content, bool completed);
+    event TaskCompleted(uint id, bool completed);
+    constructor() {
+      createTask("Check out Metamask");
+    }
+
+    function createTask(string memory _content) public {
+      taskCount++;
+      tasks[taskCount] = Task(taskCount, _content, false);
+
+      emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+      tasks[_id].completed = !tasks[_id].completed;
+      emit TaskCompleted(_id, tasks[_id].completed);
+    }
+
+
 }
